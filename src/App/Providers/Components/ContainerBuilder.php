fc572ddb92ef28455d4722f2058de1330fd32d56
Isaac <isaac.musashi@gmail.com>
2016-05-10 22:30:46 -0300

    add prod.yml

--- src/App/Providers/Components/ContainerBuilder.php
+++ src/App/Providers/Components/ContainerBuilder.php
@@ -24,15 +24,17 @@ class ContainerBuilder implements ServiceProviderInterface
     public function register(Application $app)
     {
         $container = new ContainerBuilderSymfony();
+
         $loader = new YamlFileLoader($container, new FileLocator(ROOT_PATH . '/config'));
         $loader->load('config.yml');
         $loader->load('deve.yml');
+
+        if (getenv('environment') === 'prod') {
+            $loader->load('prod.yml');
+        }
+
         $loader = new YamlFileLoader($container, new FileLocator(ROOT_PATH . '/src/Resources'));
         $loader->load('services.yml');
-
-//        if (!getenv('environment')) {
-//            $loader->load('prod.yml');
-//        }
 
         $app['container'] = $container;
     }
